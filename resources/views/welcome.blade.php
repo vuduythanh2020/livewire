d6762c5a34252c8381ae8bad99949d58a6716d95
Vu Duy Thanh <vuduythanh@gobiz.vn>
2023-01-13 10:54:29 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<html lang="en">
+<head>
+    <title>
+        Livewire
+    </title>
+    @livewireStyles
+</head>
+<body>
+    <livewire:counter />
+
+    @livewireScripts
+</body>
+</html>
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./resources/views/*.{html,js,php}", "./resources/views/livewire/*.{html,js,php}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
